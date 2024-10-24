b4c5dae36db216e6867572ef9cb232b9e5d6ec91
Adajli <ajli@smith.edu>
2024-10-24 10:57:31 -0400

    Added  methods to the function with modifications.

--- House.java
+++ House.java
@@ -1,12 +1,38 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
+import java.util.ArrayList;
+
+public class House extends Building{
+  private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+
+ 
+  public House(boolean hasDiningRoom) {
+    this.residents = new ArrayList<String>();
+    super("Home", "10 Meadowview Road",2);
+    residents = new ArrayList<String>();
+    hasDiningRoom = true;
     System.out.println("You have built a house: 🏠");
   }
+  public boolean hasDiningRoom(){
+    return hasDiningRoom;
+  }
+  public int nResidents(){
+    return residents.size();
+  }
+  public void moveIn(String name){
+     residents.add(name);
+  }
 
+  public void moveOut(String name){
+    if(residents.contains(name)){
+      residents.remove(name);
+    }else{
+      System.err.println("Can't remove that person");
+    }
+  }
   public static void main(String[] args) {
-    new House();
+    new House(true);
   }
 
 }
--- Library.java
+++ Library.java
@@ -1,7 +1,13 @@
 /* This is a stub for the Library class */
+
+import java.util.Hashtable;
+
 public class Library {
+    //Attributes
+    private Hashtable<String, Boolean> collection;
 
     public Library() {
+      super();
       System.out.println("You have built a library: 📖");
     }
   
