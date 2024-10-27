4c0ae4a9b1a27ae39069623dd8e9ee026368aa40
Adajli <ajli@smith.edu>
2024-10-27 09:39:22 -0400

    Added another statement to test if the function is working.

--- House.java
+++ House.java
@@ -1,84 +1,100 @@
 /* This is the full House class */
 
 import java.util.ArrayList;
-/** 
+
+/**
  * This is the house clas
+ * 
  * @author Ada Li
  * @since October 24, 2024
-*/
-public class House extends Building{
-  //House attributes
+ */
+public class House extends Building {
+  // House attributes
   private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
   private boolean hasDiningRoom;
 
   /**
    * House constructor
+   * 
    * @param name
    * @param address
    * @param nFloors
    * @param hasDiningRoom
    */
-  public House(String name, String address, int nFloors,boolean hasDiningRoom) {
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
     this.residents = new ArrayList<String>();
-    super(name, address,nFloors);
+    super(name, address, nFloors);
     this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
   }
+
   /**
    * Accessor to check if the house has dining room
+   * 
    * @return hasDiningRoom
    */
-  public boolean hasDiningRoom(){
+  public boolean hasDiningRoom() {
     return hasDiningRoom;
   }
+
   /**
    * Accessor for number of residents of house
+   * 
    * @return number of residents
    */
-  public int nResidents(){
+  public int nResidents() {
     return residents.size();
   }
+
   /**
    * Move in function
+   * 
    * @param name
    */
-  public void moveIn(String name){
-    if(residents.contains(name)){
+  public void moveIn(String name) {
+    if (residents.contains(name)) {
       throw new RuntimeException("Can't add that person");
-    }else{
+    } else {
       residents.add(name);
     }
   }
+
   /**
    * Move out function
+   * 
    * @param name
    */
-  public void moveOut(String name){
-    if(residents.contains(name)){
+  public void moveOut(String name) {
+    if (residents.contains(name)) {
       residents.remove(name);
-    }else{
+    } else {
       throw new RuntimeException("Can't remove that person");
     }
   }
+
   /**
    * Function to check if the person is the resident of house
+   * 
    * @param person
    * @return isResident
    */
-  public boolean isResident(String person){
+  public boolean isResident(String person) {
     boolean isResident = false;
-    if(residents.contains(person)){
+    if (residents.contains(person)) {
       isResident = true;
     }
     return isResident;
-  }  
+  }
+
   public static void main(String[] args) {
-    House myHome = new House("My Home", "10 Meadowview Road Basking Ridge, NJ", 2,true);
+    House myHome = new House("My Home", "10 Meadowview Road Basking Ridge, NJ", 2, true);
     myHome.moveIn("Ada Li");
     myHome.moveIn("Hong Li");
     myHome.moveIn("Jin Chen");
+    myHome.moveIn("Arthur Li");
     System.out.println(myHome.isResident("Ada Li"));
-    System.out.println(myHome.nResidents()); 
+    System.out.println(myHome.hasDiningRoom());
+    System.out.println(myHome.nResidents());
   }
 
 }
